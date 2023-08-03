07820b3b0ca4b4c35afcb16485bf50900efaabaf
Nurunnahar <nurunnahar9081@gmail.com>
2023-08-04 02:35:29 +0600

    Login Logout Registration reset-password profile-update

---
+++ LOGIN LOGOUT  REGISTRATION   RESET-PASSWORD   PROFILE-UPDATE/point_of_sell/routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Middleware\TokenVerificationMiddleware;
+
+// Web API Routes
+Route::post('/user-registration',[UserController::class,'UserRegistration']);
+Route::post('/user-login',[UserController::class,'UserLogin']);
+Route::post('/send-otp',[UserController::class,'SendOTPCode']);
+Route::post('/verify-otp',[UserController::class,'VerifyOTP']);
+Route::post('/reset-password',[UserController::class,'ResetPassword'])->middleware([TokenVerificationMiddleware::class]);
+Route::get('/user-profile',[UserController::class,'UserProfile'])->middleware([TokenVerificationMiddleware::class]);
+Route::post('/user-update',[UserController::class,'UserUpdate'])->middleware([TokenVerificationMiddleware::class]);
+
+
+
+// User Logout
+Route::get('/logout',[UserController::class,'UserLogout']);
+
+// Page Routes
+Route::get('/userLogin',[UserController::class,'LoginPage']);
+Route::get('/userRegistration',[UserController::class,'RegistrationPage']);
+Route::get('/sendOtp',[UserController::class,'SendOtpPage']);
+Route::get('/verifyOtp',[UserController::class,'VerifyOTPPage']);
+Route::get('/resetPassword',[UserController::class,'ResetPasswordPage'])->middleware([TokenVerificationMiddleware::class]);
+Route::get('/dashboard',[DashboardController::class,'DashboardPage'])->middleware([TokenVerificationMiddleware::class]);
+Route::get('/userProfile',[UserController::class,'ProfilePage'])->middleware([TokenVerificationMiddleware::class]);
+
+
+
