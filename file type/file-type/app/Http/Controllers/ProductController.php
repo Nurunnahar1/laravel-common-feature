3b7d013eccd714e1bc90f2a0a822086552614fbe
Nurunnahar <nurunnahar9081@gmail.com>
2023-08-26 01:31:18 +0600

    file type and file storage

--- file type/file-type/app/Http/Controllers/ProductController.php
+++ file type/file-type/app/Http/Controllers/ProductController.php
@@ -5,9 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use App\Models\Category;
 use App\Models\SubCategory;
- 
+
 use Illuminate\Http\Request;
 use App\Http\Requests\UpdateProductRequest;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
@@ -35,7 +36,30 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        dd($request->all());
+        // dd($request->all());
+
+        $request->validate([
+             'category_id'=>'required|numeric',
+             'subcategory_id'=>'required|numeric',
+             'name'=>'required|string|max:255',
+             'price'=>'required|numeric|min:0',
+             'description'=>'nullable|string',
+            //  'image'=>'bail|required|image|mines:png,jpg,jepg|max:1024',
+             'image'=>'bail|required|image|max:1024',
+        ]);
+        // dd($request->hasFile('image'));
+        $file_exits=$request->hasFile('image');
+        if($file_exits){
+            $file= $request->file('image');
+            $file_type=$file->getClientMimeType();
+            $file_ext=$file->getClientOriginalExtension();
+            $file_ore_name= $file->getClientOriginalName();
+
+            // dd($file,$file_type,$file_ext,$file_ore_name);
+            // dump($file->store('image'));
+            // dump(Storage::disk('public')->put('image',$file));
+            dump(Storage::putFileAs('product_image',$file,'new_product_1'.'.'.$file->getClientOriginalExtension()));
+        }
     }
 
     /**
